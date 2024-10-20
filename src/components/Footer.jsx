4869da73cebeac2026f98aebd47648d86967a50d
Aswin-04 <e.aswin004@gmail.com>
2024-10-20 16:46:20 +0530

    complete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ import { quickLinks } from "../constants";
 import { Link } from "react-router-dom";
 import { Mail } from "lucide-react";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import PhoneIcon from "@mui/icons-material/Phone";
+import { Phone } from "lucide-react";
 
 const Footer = () => {
   return (
@@ -75,7 +80,83 @@ const Footer = () => {
           </ul>
         </div>
       </div>
+
+
+      <div className="flex flex-col md:justify-center md:items-center border padding-x py-8 border-x-0 border-gray-300 md:text-center shadow-lg">
+
+        <div className="flex gap-2">
+          <a
+            href="https://www.instagram.com/sairam_mech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramIcon
+              fontSize="large"
+              sx={{
+                color: "#334155",
+                transition: "color 0.25s ease-in-out",
+                "&:hover": {
+                  color: "#020617", 
+                 },
+              }}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/sairam-mechanical-engineering/posts/?feedView=all"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInIcon
+              fontSize="large"
+              sx={{
+                color: "#334155",
+                transition: "color 0.25s ease-in-out",
+                "&:hover": {
+                  color: "#020617",                
+                },
+              }}
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/@sairammechanicalofficial8424"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <YouTubeIcon
+              sx={{
+                color: "#334155",
+                fontSize: 46,
+                paddingBottom: 1,
+                transition: "color 0.25s ease-in-out",
+                "&:hover": {
+                  color: "#020617",                
+                },
+              }}
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/sairamecmech?mibextid=qi2Omg&rdid=6tTnLAW1kU7pwNuO&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FmPf3KkcC7Kqgxpet%2F%3Fmibextid%3Dqi2Omg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FacebookIcon
+              fontSize="large"
+              sx={{
+                color: "#334155",
+                // fontSize: 46,
+                // paddingBottom: 1,
+                transition: "color 0.25s ease-in-out",
+                "&:hover": {
+                  color: "#020617",
+                },
+              }}
+            />
+          </a>
+        </div>
+      </div>
     </footer>
+
+
   );
 };
 
