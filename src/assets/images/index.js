3507e98a0f28624db5cf437dd11cec7e0db96480
Aswin-04 <e.aswin004@gmail.com>
2024-10-20 13:22:19 +0530

    update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,11 @@
 import mainPoster from './main-poster.jpg'
 import clgImg from './clg-img.jpg'
 import departmentImg from './department.jpeg'
+import sairamIcon from './sairam-icon.png'
 
 export {
   mainPoster,
   clgImg,
-  departmentImg
+  departmentImg,
+  sairamIcon
 }
