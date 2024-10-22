5af29a3ed359b61e99a76471c01a5b9a57937ccb
Aswin-04 <e.aswin004@gmail.com>
2024-10-22 14:51:38 +0530

    change font in Heading comp

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = ({ title }) => {
   return (
 
-    <div className="bg-bgd py-6 md:py-12">
+    <div className="bg-bgd py-6 md:py-12 font-montserrat">
       <h1 className="h1 uppercase text-center font-bold text-4xl text-tx leading-8">
         {title}
       </h1>
