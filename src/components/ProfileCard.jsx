afea63d740d6bb18e5e196d747d86764dbda04ee
Aswin-04 <e.aswin004@gmail.com>
2024-10-23 11:14:23 +0530

    Complete speakers page

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,23 @@
+  import LinkedInIcon from '@mui/icons-material/LinkedIn';
+  import { blue } from '@mui/material/colors';
+  
+
+  const ProfileCard = ({profile}) => {
+    return (
+      <div  className=" profile-card relative font-sans flex flex-col items-center mt-10 w-[320px] pt-6 pb-8 rounded-3xl text-center bg-card backdrop-blur-lg shadow">
+        <div className="bg-bgd mb-4  rounded-full w-[170px] h-[170px] flex items-center justify-center">
+          <img src={profile.img} alt={profile.imgAlt}  className="rounded-full w-[160px] h-[160px]"/>
+        </div>
+        <h3 className="h3-text">{profile.name}</h3>
+        {profile.year && (
+          <p className="mb-4 text-sm">{profile.year}</p>
+        )}
+        <a href={profile.linkedIn} target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon sx={{ color: blue[500] }} fontSize="large"/>
+        </a>
+
+      </div>
+    )
+  }
+
+  export default ProfileCard
--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -1,10 +1,31 @@
-
+import Navbar from "../components/Navbar";
+import MainPoster from "../components/MainPoster";
+import Footer from "../components/Footer";
+import ProfileCard from "../components/ProfileCard";
+import Heading from '../components/Heading'
+import rpImg from '../assets/images/speakers/RP.jpeg'
 const Speakers = () => {
   return (
     <div>
-      
+      <Navbar />
+      <MainPoster />
+      <div className="my-10">
+        <Heading title={'Speakers'}/>
+      </div>
+      <div className="my-20 flex justify-center items-center">
+        <ProfileCard
+          profile={{
+            img: rpImg,
+            imgAlt: "rpSirImg",
+            name: "Dr. A. Rajendra Prasad",
+            year: "Dean of Student Affairs",
+            linkedIn: "https://www.linkedin.com/in/arprasad225/",
+          }}
+        ></ProfileCard>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Speakers
+export default Speakers;
