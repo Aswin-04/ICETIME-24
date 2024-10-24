9193edc1b641eda4a0c7597c69034f22da6ae2da
Aswin-04 <e.aswin004@gmail.com>
2024-10-24 10:06:46 +0530

    add registration fee component

--- src/components/RegistrationFeeTable.jsx
+++ src/components/RegistrationFeeTable.jsx
@@ -1,8 +1,7 @@
-
 const RegistrationFeeTable = () => {
   return (
     <div className="overflow-x-auto">
-      <table className="w-full  md:max-w-2xl table-auto border-collapse border border-gray-300">
+      <table className="w-full md:max-w-3xl table-auto border-collapse border border-gray-300">
         <thead>
           <tr className="bg-gray-100">
             <th className="border border-gray-300 px-4 py-2 text-left">Category</th>
@@ -12,14 +11,29 @@ const RegistrationFeeTable = () => {
         </thead>
         <tbody>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">Students</td>
-            <td className="border border-gray-300 px-4 py-2">Rs. 1000</td>
-            <td className="border border-gray-300 px-4 py-2">$ 20</td>
+            <td className="border border-gray-300 px-4 py-2">Industry (without Journal Publication)</td>
+            <td className="border border-gray-300 px-4 py-2">Rs. 3000</td>
+            <td className="border border-gray-300 px-4 py-2">-</td>
           </tr>
           <tr>
-            <td className="border border-gray-300 px-4 py-2">Industry delegates, Academicians</td>
-            <td className="border border-gray-300 px-4 py-2">Rs. 2500</td>
-            <td className="border border-gray-300 px-4 py-2">$ 30</td>
+            <td className="border border-gray-300 px-4 py-2">Govt./Academic/Research Institutions (without Journal Publication)</td>
+            <td className="border border-gray-300 px-4 py-2">Rs. 1500</td>
+            <td className="border border-gray-300 px-4 py-2">USD xxx</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">Students/Research Scholars (without Journal Publication)</td>
+            <td className="border border-gray-300 px-4 py-2">-</td>
+            <td className="border border-gray-300 px-4 py-2">USD xxx</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">Listeners and Accompanying Persons</td>
+            <td className="border border-gray-300 px-4 py-2">Rs. 1000</td>
+            <td className="border border-gray-300 px-4 py-2">USD xxx</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">Scopus Journal Publication</td>
+            <td className="border border-gray-300 px-4 py-2">Rs. 6500</td>
+            <td className="border border-gray-300 px-4 py-2">USD xxx</td>
           </tr>
         </tbody>
       </table>
