d5f7fd35fff276025510d20bd1d64ba2644c496f
Aswin-04 <e.aswin004@gmail.com>
2024-10-25 10:39:24 +0530

    fix typo in about the institution content

--- src/constants/index.js
+++ src/constants/index.js
@@ -119,11 +119,11 @@ Proceedings of ICGEST 2024 will be published in the *E3S Web of Conferences* (Op
     title: "About the Institution",
     content: `
 
-**Sri Sai Ram Engineering College**, a BIS/EOMS ISO 21001:2018 certified Institution, was established by Founder Chairman MJF. Ln. Leo Muthu in 1995. All the eligible Engineering programs have been accredited by the National Board of Accreditation (NBA) and also accredited by NAAC with an ‘A+’ grade. The College is situated in a sylvan environment and offers sixteen UG courses and six PG courses in Engineering, Technology, and Management, with a strength of over 5000 students.
+**Sri Sai Ram Engineering College**, a BIS/EOMS ISO 21001:2018 certified Institution, was established by Founder Chairman MJF. Ln. Leo Muthu in 1995. All the eligible Engineering programs have been accredited by the National Board of Accreditation (NBA) and also accredited by NAAC with an ‘A+’ grade. The College is situated in a sylvan environment and offers sixteen UG courses and six PG courses in Engineering, Technology, and Management, with a strength of over 8000 students.
 
 The Institution has grown and is recognized among the reputed Institutions in Tamil Nadu in a short span of time, thanks to the highest degree of commitment and enthusiasm from the staff and management. With the motto, **“Prosperity through quality education,”** the institution disseminates knowledge and entrepreneurship skills among the students and strives to achieve excellence in the fields of Technical, Computer, and Management education. The College is among the top Colleges in Tamil Nadu.
 
-In the NIRF ranking among engineering Colleges in India, Sri Sairam Engineering College, Chennai, has been positioned in 157th place in 2022. The College has adopted the **‘Sustainable Development Goals (SDG)’** in all its activities to create awareness and build a sense of responsibility towards society in the minds of budding engineers, with the aim of building a better world. The College has highly qualified and dedicated professors guiding various socially relevant projects funded through state and central government agencies like DST, AICTE, DRDO, CSIR, and MHRD, and publishing research outputs in various national and international journals.
+In the NIRF ranking among engineering Colleges in India, Sri Sairam Engineering College, Chennai, has been positioned since 2017. The College has adopted the **‘Sustainable Development Goals (SDG)’** in all its activities to create awareness and build a sense of responsibility towards society in the minds of budding engineers, with the aim of building a better world. The College has highly qualified and dedicated Professors guiding various socially relevant projects funded through state and central government agencies like DST, AICTE, DRDO, CSIR, and ME, and also publishing research outputs in various national and international journals.
 
 `,
     img: clgImg,
@@ -139,7 +139,7 @@ The Department of Mechanical Engineering was established in the year 1995. It ha
 
 The Master of Engineering (M.E) - CAD/CAM program focuses on preparing graduate students in the multi-disciplinary profession of computer-aided design and manufacturing for the challenges and opportunities that abound in modern technology and society. The Department has been recognized as a Research Centre by Anna University and has several research centers, including the Material Research Centre, Clean Energy Centre, and Reverse Engineering & CAE Centre.
 
-The Department has signed MoUs with organizations like TIEMA, AIEMA, COINDIA, and many leading industries. It also has professional societies such as SAE, ISTE, IEI, ISHRAE, IWS, IEEE, and QCFI. Our students have bagged awards from SAE events like M-BAJA, E-BAJA, SUPRA, TIFAN, Effi-Cycle, Bicycle, and other events like Solaris, Go-Kart, E-bike, and Dirt Bike.
+The Department has signed MoUs with organizations like TIEMA, AIEMA, COINDIA, and many leading industries. It also has professional societies such as SAE, ISTE, IEI, ISHRAE, IWS, IEEE, and QCFI. Our students have bagged awards from SAE events like M-BAJA, E-BAJA, SUPRA, TIFAN, Effi-Cycle, Bicycle, and other events like Solaris, Go-Kart, E-bike, and Dirt Bike. The department established a Centre of Excellence (COE) in collaboration with Delphi-TVS Technologies Ltd. to train students in advanced fuel injection systems.
 
 `,
     img: departmentImg,
