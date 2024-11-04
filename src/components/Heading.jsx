331df5a8a88c8149ca13d15dd687b047dfcf5227
Aswin-04 <e.aswin004@gmail.com>
2024-11-04 18:56:21 +0530

    change header clr

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,8 @@
 const Heading = ({ title }) => {
   return (
 
-    <div className="bg-bgd py-6 md:py-12 font-montserrat">
-      <h1 className="h1 uppercase text-center font-bold text-4xl text-tx leading-8">
+    <div className="bg-bgd py-6 md:py-12 ">
+      <h1 className="h1 uppercase text-center font-bold text-4xl leading-8 text-black/80">
         {title}
       </h1>
     </div>
