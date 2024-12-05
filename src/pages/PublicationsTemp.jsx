dd25bd51cecb88b906ad3c38c99a219a9df764b8
Aswin-04 <e.aswin004@gmail.com>
2024-12-05 11:37:22 +0530

    Add spacing in temp Publications

--- src/pages/PublicationsTemp.jsx
+++ src/pages/PublicationsTemp.jsx
@@ -18,8 +18,8 @@ const PublicationsTemp = () => {
         <Heading title={"Publications"}></Heading>
       </div>
       <div className="bg-slate-100">
-        <div className="max-container mb-20 lg:mb-32 py-10 flex flex-col items-center gap-12">
-          <p className='text-2xl font-medium'>Accepted papers will be published in one of the journals listed below</p>
+        <div className="max-container mb-20 lg:mb-32 py-10 flex flex-col items-center gap-12 px-6">
+          <p className='text-2xl max-sm:text-lg font-medium text-center'>Accepted papers will be published in one of the journals listed below</p>
           <img src={aipLogo} alt="aipLogo" width={500} height={500}/>
           <img src={etsLogo} alt="e3sLogo" width={500} height={500}/>
           <img src={springerLogo} alt="springerLogo" width={500} height={500}/>
